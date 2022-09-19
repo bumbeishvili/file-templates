e18e57732d6f849f65d507ae47c0b624abb95d58
David  Bumbeishvili <davit.bumbeishvili@gmail.com>
2022-09-19 18:01:30 +0400

    initial template files

---
+++ svelte/chart.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
+	import { API } from '../../shared/API';
+
+	import { Chart } from './chart.d3';
+
+	export let data = {
+		name: 'test',
+		color: 'red'
+	};
+	let classes = '';
+	export { classes as class };
+
+	let chart;
+	let chartContainer;
+
+	$: {
+		if (chart) {
+			chart.data(data).render();
+		}
+	}
+
+	onMount(() => {
+		chart = new Chart(data)
+			.svgHeight(400)
+			.svgWidth(window.innerWidth - 30)
+			.container(chartContainer)
+			.data(data)
+			.render();
+	});
+</script>
+
+<div bind:this={chartContainer} />
